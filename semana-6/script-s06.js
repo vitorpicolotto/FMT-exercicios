dbff512871484a25ea26514f16c69d49a307a478
vitorpicolotto <picolottovitor@gmail.com>
2024-02-26 17:17:34 -0300

    Exercicio 9 - adicionei um evento que, ao passar o mouse sobre o texto Cor: Laranja, a cor é alterada e, quando o mouse sai, a cor volta a sua padrão.

--- semana-6/script-s06.js
+++ semana-6/script-s06.js
@@ -1,1 +1,16 @@
-alert("Bem-vindo(a) ao perfil do Cenoura no CatBook, a rede social para gatos!")
+//Atividade 7 - Adicionando um alert
+//alert("Bem-vindo(a) ao perfil do Cenoura no CatBook, a rede social para gatos!")
+
+//Atividade 8 - Manipulando o DOM
+//Coloquei um evento de mudar a cor do texto "COR: LARANJA" ao passar o mouse por cima e voltar a cor original depois de tirar o mouse
+
+const mudarCorDoTexto = document.getElementById("cor");
+const corAtual = mudarCorDoTexto.style.color;
+
+mudarCorDoTexto.addEventListener('mouseover',  () => {
+    mudarCorDoTexto.style.color='orange';
+})
+
+mudarCorDoTexto.addEventListener( 'mouseout' , ()=>{
+    mudarCorDoTexto.style.color=corAtual;
+})
