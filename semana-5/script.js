7555e20971ed0dc39b3c1f01a69c5ddd75f313fe
vitorpicolotto <picolottovitor@gmail.com>
2024-03-26 19:39:56 -0300

    Criando os documentos HTML, CSS e JS para os exercicios da semana 5 do FMT. Também Já fiz a estilização da página com o CSS, criei os elementos solicitados para o exercício 1 e o objeto que estarei trabalhando nos próximos exercícios.

---
+++ semana-5/script.js
@@ -0,0 +1,8 @@
+const jogos = [
+    {Jogo: "Super Mario Wonder", Plataforma: "Nintendo Switch", Valor:"R$ 300,00", Imagem: "imgs/supermariowonder.jpg"},
+    {Jogo: "The Legend of Zelda: Breath of the Wild", Plataforma: "Nintendo Switch", Valor:"R$ 259,90", Imagem: "imgs/tloz.jpg"},
+    {Jogo: "The Legend of Zelda: Tears of the Kingdom", Plataforma: "Nintendo Switch", Valor:"R$ 350,00", Imagem: "imgs/totk.jpg"},
+    {Jogo: "Donkey Kong Country", Plataforma: "Nintendo Switch", Valor: "R$200,00", Imagem: "imgs/dkc.jpg"},
+    {Jogo: "Super Mario Kart 8", Plataforma: "Nintendo Switch", Valor: "R$179,90", Imagem: "imgs/smkd8.jpg"}
+];
+
