b323e36fb4e14a8dd49a0836d5e3db7a7e662b28
vitorpicolotto <picolottovitor@gmail.com>
2024-03-28 17:11:40 -0300

    Inserindo o nome e valor do jogo de forma dinâmica pelo javascript, mais algumas mudanças no index e estilos.

--- semana-5/script.js
+++ semana-5/script.js
@@ -6,3 +6,26 @@ const jogos = [
     {Jogo: "Super Mario Kart 8", Plataforma: "Nintendo Switch", Valor: "R$179,90", Imagem: "imgs/smkd8.jpg"}
 ];
 
+
+//Exercício 2: adicionar um evento de clique para consultar o valor do produto digitado pelo consumidor
+
+const botaoBuscar = document.getElementById("formulario__botao");
+botaoBuscar.addEventListener( 'click', function(evento){
+    evento.preventDefault(); //impede que a página seja atualizada após clicar no botão
+});
+
+function  buscaValor() {
+    let jogoBuscado = document.getElementById("principal__jogos-valor");
+    let jogo = document.getElementById("pesquisa").value;
+    let resultadoBusca = jogos.find((element) => element.Jogo === jogo);
+    if (resultadoBusca) {
+        jogoBuscado.textContent = `O jogo ${resultadoBusca.Jogo} para ${resultadoBusca.Plataforma} tem o valor de R$ ${resultadoBusca.Valor}.`;
+    } else{
+        jogoBuscado.textContent = "Esse jogo não existe! Tente outro nome.";
+    }
+}
+
+//Exercicio 3: adicionar um evento de clique no botão comprar. O objetivo é que as informações do jogo apareçam na área do carrinho.
+//precisa criar um array vazio para o carrinho e dar push para inserir os dados nele
+
+
