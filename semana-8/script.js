4ae0771e9dc374047d042387da1b246c16044611
vitorpicolotto <picolottovitor@gmail.com>
2024-04-05 15:48:28 -0300

    finalizando os exercícios 8, 9 e 10

--- semana-8/script.js
+++ semana-8/script.js
@@ -53,5 +53,42 @@ function removeClass(){
 
 //Exercícios 8, 9 e 10 - fiz a adição e remoção de atributos no exercício 10, onde devíamos criar uma lista de tarefas de forma funcional
 
+function adicionarTarefa(){
+    let tarefaInput = document.getElementById("tarefaInput");
+    let textoTarefa = tarefaInput.value;
+    let listaTarefa = document.getElementById("listaTarefa");
 
+    //criando elemento de lista 
+    let itemTarefa = document.createElement('li');
+    itemTarefa.textContent = textoTarefa;
+
+    //Evento de clique para marcar como concluída
+    itemTarefa.addEventListener('click', function() {
+        if(this.getAttribute("item-marcado") === "true"){
+            this.removeAttribute("item-marcado");
+            this.classList.remove("concluida");
+        } else {
+            this.setAttribute( "item-marcado", "true");
+            this.classList.add("concluida");
+        }
+    })
+
+    //botão para remover
+    let botaoRemover  = document.createElement('button');
+    botaoRemover.textContent = "Remover item"
+    botaoRemover.addEventListener('click', function (evento) {
+        evento.stopPropagation( ); //impede que o evento se propague pela lista
+        listaTarefa.removeChild(itemTarefa);
+    })
+
+    //adicionando botão de remover o  item da lista
+    itemTarefa.appendChild(botaoRemover);
+
+    //adicionando item a lista de tarefa
+    listaTarefa.appendChild(itemTarefa);
+
+    //limpar o input
+    tarefaInput.value="";
+
+}
 
